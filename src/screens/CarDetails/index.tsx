8fbff88317c3da188f67550fb6dabf02b8eb7a5a
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2021-10-16 14:37:05 -0300

    Component:ImageSlider (check)

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
    Container,
    Header,
+   CarImages,
 } from './styles';
 
 export function CarDetails(){
@@ -13,7 +15,12 @@ export function CarDetails(){
             <BackButton onPress={() => {}} />
         </Header>
 
-
+        <CarImages>
+          <ImageSlider 
+          imagesUrl={['https://img2.gratispng.com/20180202/gzw/kisspng-2016-ferrari-488-spider-sports-car-luxury-vehicle-blue-ferrari-488-spider-car-5a740f6e14df79.1923682815175555660855.jpg']}
+          />
+        </CarImages>
+        
     </Container>
   );
 }
