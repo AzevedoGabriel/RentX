e90df7cb6e4f460d739f8963ffc80ca2e346dbab
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2021-10-16 08:29:24 -0300

    Home:header (check)

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,14 +1,36 @@
 import React from 'react';
+import {StatusBar} from 'react-native';
+import { RFValue } from 'react-native-responsive-fontsize';
+
+import Logo from '../../assets/lgo.svg';
 
 import {
    Container,
-   Title,
+   Header,
+   TotalCars,
+   HeaderContent,
 } from './styles';
 
 export function Home(){
   return (
     <Container>
-        <Title>Home</Title>
+        <StatusBar 
+          barStyle='light-content'
+          backgroundColor="transparent"
+          translucent
+        />
+        <Header>
+          <HeaderContent>
+            <Logo 
+              width={RFValue(108)}
+              height={RFValue(12)}
+            />
+            <TotalCars>
+              Total de 12 Carros
+            </TotalCars>
+          </HeaderContent>
+          
+        </Header>
     </Container>
   );
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,3 +1,4 @@
+import { RFValue } from 'react-native-responsive-fontsize';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
@@ -8,7 +9,25 @@ export const Container = styled.View`
    background-color: ${({theme}) => theme.colors.background_primary};
 `;
 
-export const Title = styled.Text`
-   font-size: 30px;
-   font-family: ${({theme}) => theme.fonts.secondary_600};;
-`;''
+export const Header = styled.View`
+   width: 100%;
+   height: 113px;
+
+   background-color: ${({theme}) => theme.colors.header};
+
+   justify-content: flex-end;
+   padding: 32px 24px;
+`;
+
+export const HeaderContent = styled.View`
+   align-items: center;
+   justify-content: space-between;
+   flex-direction: row;
+`;
+
+
+export const TotalCars = styled.Text`
+   font-size: ${RFValue(15)}px;
+   font-family: ${({theme}) => theme.fonts.primary_400};
+   color: ${({theme}) => theme.colors.text};
+`;
